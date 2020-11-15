993a93d7b3630c227d1ee7f3aada1bed8da04daa
Wahid-Mujawar <abdulwahid0218@gmail.com>
2020-11-14 16:01:29 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -24,23 +24,23 @@ class App extends Component {
 
   }
 
-  getResumeData(){
-    $.ajax({
-      url:'/resumeData.json',
-      dataType:'json',
-      cache: false,
-      success: function(data){
-        this.setState({resumeData: data});
-      }.bind(this),
-      error: function(xhr, status, err){
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
+  // getResumeData(){
+  //   $.ajax({
+  //     url:'/resumeData.json',
+  //     dataType:'json',
+  //     cache: false,
+  //     success: function(data){
+  //       this.setState({resumeData: data});
+  //     }.bind(this),
+  //     error: function(xhr, status, err){
+  //       console.log(err);
+  //       alert(err);
+  //     }
+  //   });
+  // }
 
   componentDidMount(){
-    this.getResumeData();
+    // this.getResumeData();
   }
 
   render() {
